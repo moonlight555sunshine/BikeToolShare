7ae05929c0816ecc4957cda8345101ffc9c6064d
Your Name <you@example.com>
2025-07-25 16:45:14 +0200

    added the ability to add images

--- BikeToolsShare/urls.py
+++ BikeToolsShare/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-
+from . import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='base.html'), name="home"),
     path('tool/', include('tool.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
