202d08db1d6bdee5a9ad8a7b2d917aa5ff95f3b5
Your Name <you@example.com>
2025-07-29 14:41:16 +0200

    create tool app  models and add to admin

--- tool/admin.py
+++ tool/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from tool.models import Tool, Category
+
 # Register your models here.
+admin.site.register(Tool)
+admin.site.register(Category)
--- tool/models.py
+++ tool/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import ManyToManyField
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Tool(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = ManyToManyField(Category, related_name='tools')
+    is_available = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tools_owner')
+    image = models.ImageField(upload_to='uploads/tool/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.owner.username})"
