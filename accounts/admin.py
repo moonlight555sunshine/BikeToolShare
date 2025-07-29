d1e8df7fbfab10b9d44a00714a18df51e65af03d
Your Name <you@example.com>
2025-07-29 15:02:07 +0200

    create user profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import UserProfile
+from django.contrib.auth.models import User
 
-# Register your models here.
+admin.site.register(UserProfile)
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "email", "first_name", "last_name"]
+    inlines = [UserProfileInline]
+
+# # unregister the old way
+# admin.site.unregister(User)
+#
+# # register the new way
+# admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(auto_now=True)
+    country = models.CharField(max_length=50, default='Poland')
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    address = models.CharField(max_length=50, default='', blank=True)
+    zipcode = models.CharField(max_length=12, default='', blank=True)
+    phone = PhoneNumberField(blank=True, region='PL')
+    profile_picture = models.ImageField(upload_to='uploads/profile/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = UserProfile(user=instance)
+        user_profile.save()
+post_save.connect(create_user_profile, sender=User)
