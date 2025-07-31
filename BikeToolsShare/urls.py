fbf715f7931f0851d0e944377203d999e94b6c33
Your Name <you@example.com>
2025-07-31 21:04:26 +0200

    added about page

--- BikeToolsShare/urls.py
+++ BikeToolsShare/urls.py
@@ -21,6 +21,7 @@ from . import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='base.html'), name="home"),
+    path('', TemplateView.as_view(template_name='home.html'), name="home"),
+    path('about/', TemplateView.as_view(template_name='about.html'), name="about"),
     path('tool/', include('tool.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
