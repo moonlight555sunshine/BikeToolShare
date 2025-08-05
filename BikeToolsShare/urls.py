d201af9d358d1f087d2a7389882abdd660f709e5
Your Name <you@example.com>
2025-08-05 12:45:20 +0200

    was added info on home page

--- BikeToolsShare/urls.py
+++ BikeToolsShare/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+
+from tool.views import HomeView
 from . import settings
+from tool.views import HomeView
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name="home"),
+    path('', HomeView.as_view(), name="home"),
     path('about/', TemplateView.as_view(template_name='about.html'), name="about"),
     path('tool/', include('tool.urls')),
     path('accounts/', include('accounts.urls')),
--- tool/views.py
+++ tool/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from tool.models import Tool, Category
+
+
+class HomeView(View):
+    def get(self, request):
+        latest_tools = Tool.objects.all().order_by('-id')[:4]
+        categories = Category.objects.all()
+        return render(request, "home.html", {
+            'latest_tools': latest_tools,
+            'categories': categories,
+        })
