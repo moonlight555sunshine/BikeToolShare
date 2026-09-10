74e0e43b461c571fdf711ac91cf07c6420adadaa
moonlight555sunshine <moonlight555sunshine@proton.me>
2026-09-10 19:39:48 +0200

    Add validation booking date

--- booking/forms.py
+++ booking/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 from booking.models import Booking
 
 class BookingForm(forms.ModelForm):
@@ -14,3 +15,13 @@ class BookingForm(forms.ModelForm):
             'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
         }
+    def clean(self):
+        clean_data = super().clean()
+        today = timezone.localdate()
+        start_date = self.cleaned_data.get('start_date')
+        end_date = self.cleaned_data.get('end_date')
+        if start_date > end_date:
+            raise forms.ValidationError("Start date must be before end date.")
+        if start_date < today:
+            raise forms.ValidationError("Start date must be before today.")
+        return clean_data
