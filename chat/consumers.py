2ee7a98abc1a884108b254818d714fc81a8831bb
moonlight555sunshine <moonlight555sunshine@proton.me>
2026-09-17 17:46:46 +0200

    Add websocket klient

--- chat/consumers.py
+++ chat/consumers.py
@@ -11,4 +11,4 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        self.send(text_data=json.dumps({message:message}))
+        self.send(text_data=json.dumps({'message':message}))
